02c31398f9d529b92008a6fe757a1520b7c7ee7c
Tiril Mageli <tiril.mageli23@imperial.ac.uk>
2024-06-23 15:11:28 +0200

    Second NER results ready

--- knowledge_extraction/comparing_LLMs/NER.py
+++ knowledge_extraction/comparing_LLMs/NER.py
@@ -8,7 +8,10 @@ wikiann = load_dataset('wikiann', 'en')
 models = {
     'BERT': 'dslim/bert-base-NER',
     'DistilBERT': 'dslim/distilbert-NER',
-    'RoBERTa': 'Jean-Baptiste/roberta-large-ner-english'
+    'RoBERTa (large)': 'Jean-Baptiste/roberta-large-ner-english',
+    'RoBERTa (small)': 'MMG/roberta-base-ner-english',
+    'RoBERTa (large2)' : 'FacebookAI/xlm-roberta-large-finetuned-conll03-english',
+    'AlBERT': 'ArBert/albert-base-v2-finetuned-ner'
 }
 
 def join_tokens(tokens):
@@ -70,7 +73,7 @@ def get_predicted_tags(results, tokens):
     return predicted_tags
 
 def calculate_metrics(tags_pred, tags_gold):
-    tp, fp, fn = 0, 0, 0
+    tp, fp, tn, fn = 0, 0, 0, 0
 
     for pred, gold in zip(tags_pred, tags_gold):
         if pred == gold and gold != '0':
@@ -79,8 +82,10 @@ def calculate_metrics(tags_pred, tags_gold):
             fn += 1
         elif gold == '0' and pred != '0':
             fp += 1
+        elif gold == '0' and pred == '0':
+            tn += 1
     
-    return tp, fp, fn
+    return tp, fp, tn, fn
 
 def test_model(model_name, wiki_dataset, length=-1):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -95,7 +100,7 @@ def test_model(model_name, wiki_dataset, length=-1):
     if length > 0:
         iterations = length
 
-    TP, FP, FN = 0, 0, 0
+    TP, FP, TN, FN = 0, 0, 0, 0
     for i in range(iterations):
         tokens = tokens_all[i]
         true_tags = tags_all[i]
@@ -104,20 +109,22 @@ def test_model(model_name, wiki_dataset, length=-1):
         merged_result = merge_result(result)
         predicted_tags = get_predicted_tags(merged_result, tokens)
 
-        tp, fp, fn = calculate_metrics(predicted_tags, true_tags)
+        tp, fp, tn, fn = calculate_metrics(predicted_tags, true_tags)
 
         TP += tp
         FP += fp
+        TN += tn
         FN += fn
 
     end_time = time.time()
     duration = end_time - start_time
 
+    accuracy = TP/(TP + FP + TN + FN) if (TP + FP + TN + FN) else 0
     precision = TP/(TP + FP) if (TP + FP) > 0 else 0
     recall = TP/(TP + FN) if (TP + FN) > 0 else 0
     f1_score = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
 
-    return duration, precision, recall, f1_score
+    return duration, accuracy, precision, recall, f1_score
 
 data = wikiann['test'][:]
 results = {name: {} for name in models}
@@ -125,11 +132,13 @@ results = {name: {} for name in models}
 for name in models:
     print('Currently testing:', name)
     model_name = models[name]
-    duration, precision, recall, f1_score = test_model(model_name, data)
+    duration, accuracy, precision, recall, f1_score = test_model(model_name, data)
+    results[name]['Source'] = model_name
+    results[name]['Accuracy'] = accuracy
     results[name]['Precision'] = precision
     results[name]['Recall'] = recall
     results[name]['F1 Score'] = f1_score
     results[name]['Time'] = duration
 
-with open('results/240623_wikiall_results.json', 'w') as f:
+with open('results/240623_wikiall_results2.json', 'w') as f:
     json.dump(results, f, indent=4)
