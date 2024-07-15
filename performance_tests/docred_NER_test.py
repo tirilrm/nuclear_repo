eeb643fbacf61e64bb5bc83cbd5d87e60991feca
Tiril Mageli <tiril.mageli23@imperial.ac.uk>
2024-07-15 17:33:45 +0200

    Data exploration cont.

--- performance_tests/docred_NER_test.py
+++ performance_tests/docred_NER_test.py
@@ -28,16 +28,52 @@ def get_info(docred_instance):
 
     return title, text, entities, head, tail, r_id, r_text, evidence
 
-def has_approximate_match(gold, preds, threshold=0.8):
+def get_approx_match(gold, preds, threshold=0.8):
     gold_name, gold_type = gold
-
-    # Note: removed requirement that entity type must match
-    for pred_name, pred_type in preds: 
+    for pred_name, pred_type in preds:
         ratio = SequenceMatcher(None, gold_name, pred_name).ratio()
         if ratio >= threshold:
             return (pred_name, pred_type)
-    
     return None
+
+def get_results_table(tp_exact, fp_exact, fn_exact, tp_approx, fp_approx, fn_approx):
+    total_entities = tp_exact + fn_exact + tp_approx + fn_approx
+    total_correct = tp_exact + tp_approx
+
+    precision_exact = tp_exact / (tp_exact + fp_exact) 
+    recall_exact = tp_exact / (tp_exact + fn_exact) 
+    f1_score_exact = 2 * (precision_exact * recall_exact) / (precision_exact + recall_exact) 
+    accuracy_exact = tp_exact / (tp_exact + fn_exact)
+
+    precision_approx = (tp_exact + tp_approx) / (tp_exact + tp_approx + fp_approx)
+    recall_approx = (tp_exact + tp_approx) / (tp_exact + tp_approx + fn_approx) 
+    f1_score_approx = 2 * (precision_approx * recall_approx) / (precision_approx + recall_approx)
+    accuracy_approx = (tp_exact + tp_approx) / (tp_exact + tp_approx + fn_approx)
+
+    results = {
+        'exact_match': {
+            'count': tp_exact,
+            'percentage': tp_exact / total_entities,
+            'accuracy': accuracy_exact,
+            'precision': precision_exact,
+            'recall': recall_exact,
+            'f1_score': f1_score_exact
+        },
+        'approximate_match': {
+            'count': tp_approx,
+            'percentage': tp_approx / total_entities,
+            'accuracy': accuracy_approx,
+            'precision': precision_approx,
+            'recall': recall_approx,
+            'f1_score': f1_score_approx
+        },
+        'non-match': {
+            'count': fn_exact + fn_approx,
+            'percentage': (fn_exact + fn_approx) / total_entities if total_entities != 0 else 0
+        }
+    }
+
+    return results
 
 data = load_dataset('docred', trust_remote_code=True)
 datasets = [
@@ -49,27 +85,22 @@ model_name = 'dslim/distilbert-NER'
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 ner_pipeline = pipeline('ner', model=model_name, tokenizer=tokenizer)
 
-N = 3000
+N = 10
 mismatches = []
 approx_matches = []
 results_all = {}
-results_all['N'] = N
+results_all['number_of_instances'] = N
 
-correct_all = 0
-total_all = 0
-mismatch_count_all = 0
-approx_match_count_all = 0
-exact_match_count_all = 0
+tp_exact_all, fp_exact_all, fn_exact_all = 0, 0, 0
+tp_approx_all, fp_approx_all, fn_approx_all = 0, 0, 0
 
 for d, nrows in datasets:
     print(f"Investigating first {N} random instances of {d} ({nrows} instances in total)")
     mismatches.append(f"{d}:")
     approx_matches.append(f"{d}:")
-    correct = 0
-    total = 0
-    mismatch_count = 0
-    approx_match_count = 0
-    exact_match_count = 0
+
+    tp_exact, fp_exact, fn_exact = 0, 0, 0
+    tp_approx, fp_approx, fn_approx = 0, 0, 0
 
     random_permutation = np.random.permutation(nrows)[:N]
 
@@ -77,73 +108,47 @@ for d, nrows in datasets:
     for i, index in enumerate(random_permutation):
         if i % 50 == 0:
             print('Iteration:', i)
+
         title, text, ground_truth, _, _, _, _, _ = get_info(ds.iloc[index])
         result = ner_pipeline(text)
         merged_result = merge_result(result, model_name)
         predicted = combine_entities(merged_result)
         
         gold = [(gt['name'], gt['type']) for gt in ground_truth]
-        preds = [(p['word'], p['entity']) for p in predicted]
+        preds = [(p['word'], p['entity']) for p in predicted if p['entity'] in ['LOC', 'PER', 'ORG', 'MISC']]
         
         for g in gold:
-            if g[1] in ['LOC', 'PER', 'ORG', 'MISC']:
-                total += 1
-                if g in preds: 
-                    exact_match_count += 1
-                elif has_approximate_match(g, preds):
-                    approx_match_count += 1
-                    approx_matches.append(f"{g}: {has_approximate_match(g, preds)}")
-                else:
-                    mismatch_count += 1
-                    mismatches.append('gold: ' + str(g))
-                    mismatches.append('preds:')
-                    for p in preds:
-                        mismatches.append(str(p))
-                    
-    correct = exact_match_count + approx_match_count
-    accuracy = correct / total
+            if g in preds: 
+                tp_exact += 1
+                tp_approx += 1
+            elif get_approx_match(g, preds):
+                tp_approx += 1
+                fn_exact += 1
+                approx_matches.append(f"{g}: {get_approx_match(g, preds)}")
+            else:
+                fn_exact += 1
+                mismatches.append('gold: ' + str(g))
+                mismatches.append('preds:')
+                for p in preds:
+                    mismatches.append(str(p))
     
-    results = {}
-    results['accuracy'] = accuracy
+        fp_exact += len([p for p in preds if p not in gold])
+        fp_approx += len([p for p in preds if p not in gold and get_approx_match(p, gold)])
+        fn_approx += len([g for g in gold if not get_approx_match(g, preds)])
 
-    results['exact_matches'] = {}
-    results['exact_matches']['count'] = exact_match_count
-    results['exact_matches']['percent'] = exact_match_count / total
+    results_all[d] = get_results_table(tp_exact, fp_exact, fn_exact, tp_approx, fp_approx, fn_approx)
 
-    results['approx_matches'] = {}
-    results['approx_matches']['count'] = approx_match_count
-    results['approx_matches']['percent'] = approx_match_count / total
-
-    results['mismatches'] = {}
-    results['mismatches']['count'] = mismatch_count
-    results['mismatches']['percent'] = mismatch_count / total
-
-    results_all[d] = results
-    correct_all += correct
-    total_all += total
-    exact_match_count_all += exact_match_count
-    approx_match_count_all += approx_match_count
-    mismatch_count_all += mismatch_count
-
-accuracy_all = correct_all / total_all
+    tp_exact_all += tp_exact
+    fp_exact_all += fp_exact
+    fn_exact_all += fn_exact
+    tp_approx_all += tp_approx
+    fp_approx_all += fp_approx
+    fn_approx_all += fn_approx
 
 print('Calculating totals...')
 
 results = {}
-results['accuracy'] = accuracy_all
-
-results['exact_matches'] = {}
-results['exact_matches']['count'] = exact_match_count_all
-results['exact_matches']['percent'] = exact_match_count_all / total_all
-
-results['approx_matches'] = {}
-results['approx_matches']['count'] = approx_match_count_all
-results['approx_matches']['percent'] = approx_match_count_all / total_all
-
-results['mismatches'] = {}
-results['mismatches']['count'] = mismatch_count_all
-results['mismatches']['percent'] = mismatch_count_all / total_all
-results_all['total'] = results
+results_all['total'] = get_results_table(tp_exact_all, fp_exact_all, fn_exact_all, tp_approx_all, fp_approx_all, fn_approx_all)
 
 print('Done. Writing files...')
 
