bdc72f22cde9df3d1cf30bfec169b501c383f5a1
Tiril Mageli <tiril.mageli23@imperial.ac.uk>
2024-07-16 11:45:07 +0200

    Another DocRed NER test

---
+++ knowledge_extraction/relation_extraction/_RE.py
@@ -0,0 +1,120 @@
+# The functions combine_entities and merge_result are from _NER
+def combine_entities(entities):
+    combined_entities = []
+    i = 0
+    while i < len(entities):
+        current_entity = entities[i]
+        if current_entity['entity'].startswith('B-'):
+            entity_type = current_entity['entity'][2:]
+            combined_entity = {
+                'entity': entity_type,
+                'score': current_entity['score'],
+                'start': current_entity['start'],
+                'end': current_entity['end'],
+                'word': current_entity['word']
+            }
+            j = i + 1
+            while j < len(entities):
+                if entities[j]['word'] == '-':
+                    combined_entity['word'] += entities[j]['word']
+                    combined_entity['end'] = entities[j]['end']
+                    combined_entity['score'] = min(combined_entity['score'], entities[j]['score'])
+                    j += 1
+                elif entities[j]['entity'] == f'I-{entity_type}' and (entities[j]['start'] == combined_entity['end'] + 1):
+                    combined_entity['word'] += ' ' + entities[j]['word']
+                    combined_entity['end'] = entities[j]['end']
+                    combined_entity['score'] = min(combined_entity['score'], entities[j]['score'])
+                    j += 1
+                elif (entities[j-1]['word'] == '-'):
+                    combined_entity['word'] += entities[j]['word']
+                    combined_entity['end'] = entities[j]['end']
+                    combined_entity['score'] = min(combined_entity['score'], entities[j]['score'])
+                    j += 1
+                else:
+                    break
+            combined_entities.append(combined_entity)
+            i = j
+        else:
+            i += 1
+    return combined_entities
+
+def merge_result(entities, name):
+    merged_entities = []
+    current = None
+
+    if name in ['dslim/bert-base-NER', 'dslim/distilbert-NER']:
+        for entity in entities:
+            if current == None:
+                current = entity
+            else:
+                if entity['word'].startswith('##') and entity['start'] == current['end']:
+                    current['word'] += entity['word'][2:]
+                    current['end'] = entity['end']
+                    current['score'] = min(current['score'], entity['score'])
+                elif entity['start'] == current['end'] and entity['entity'][2:] == current['entity'][2:]:
+                    current['word'] += entity['word']
+                    current['end'] = entity['end']
+                    current['score'] = min(current['score'], entity['score'])
+                elif entity['start'] + 1 == current['end'] and entity['entity'][2:] == current['entity'][2:]:
+                    current['word'] += ' ' + entity['word']
+                    current['end'] = entity['end']
+                    current['score'] = min(current['score'], entity['score'])
+                else:
+                    merged_entities.append(current)
+                    current = entity
+        if current is not None:
+            merged_entities.append(current)
+    else: 
+        symbol = '▁'
+        if name in ['MMG/roberta-base-ner-english']:
+            symbol = 'Ġ'
+
+        for entity in entities:
+            if current == None:
+                current = entity
+            else:
+                if not entity['word'].startswith(symbol):
+                    current['word'] += entity['word']
+                    current['end'] = entity['end']
+                    current['score'] = min(current['score'], entity['score'])
+                else:
+                    current['word'] = current['word'][1:]
+                    merged_entities.append(current)
+                    current = entity
+        if current is not None:
+            current['word'] = current['word'][1:]
+            merged_entities.append(current)
+    return merged_entities
+
+def join_text(sents):
+    '''
+    Make a paragraph out of given sentences and words, 
+    with special rules for specific chars
+    '''
+    text = ''
+    for sent in sents:
+        sentence = ''
+        for i, word in enumerate(sent):
+            if i == 0:
+                sentence += word
+            else:
+                prev_word = sent[i-1]
+                if word.isalnum() and prev_word.isalnum():
+                    sentence += ' ' + word
+                elif prev_word in '(-':
+                    sentence += word
+                elif word in '"\'' and prev_word.isalnum():
+                    sentence += ' ' + word
+                elif word in ')-':
+                    sentence += word
+                elif word == '(':
+                    sentence += ' ' + word
+                elif not word.isalnum() and prev_word[:-1].isalnum():
+                    sentence += word
+                elif word[:-1].isalnum() and not prev_word.isalnum():
+                    sentence += ' ' + word
+                else:
+                    sentence += word
+        text += sentence + '\n'
+    
+    return text.strip()
---
+++ knowledge_extraction/relation_extraction/docred_NER_test.py
@@ -0,0 +1,162 @@
+from transformers import AutoTokenizer, pipeline
+from datasets import load_dataset
+import pandas as pd
+import itertools
+from difflib import SequenceMatcher
+import json
+import numpy as np
+
+import importlib
+import _RE
+importlib.reload(_RE)
+from _RE import join_text, merge_result, combine_entities
+
+def get_info(docred_instance):
+    title = docred_instance['title']
+    sents = [' '.join(sublist) for sublist in docred_instance['sents']]
+    sents = docred_instance['sents']
+    nested_entities = docred_instance['vertexSet']
+    entities = list(itertools.chain(*nested_entities))
+    head = docred_instance['labels']['head']
+    tail = docred_instance['labels']['tail']
+    r_id = docred_instance['labels']['relation_id']
+    r_text = docred_instance['labels']['relation_text']
+    evidence = docred_instance['labels']['evidence']
+    return title, sents, entities, head, tail, r_id, r_text, evidence
+
+def get_approx_match(gold, preds, threshold=0.8):
+    gold_name, gold_type = gold
+    for pred_name, pred_type in preds:
+        ratio = SequenceMatcher(None, gold_name, pred_name).ratio()
+        if ratio >= threshold:
+            return (pred_name, pred_type)
+    return None
+
+def get_results_table(tp_exact, fp_exact, fn_exact, tp_approx, fp_approx, fn_approx):
+    total_entities = tp_exact + fn_exact + tp_approx + fn_approx
+    total_correct = tp_exact + tp_approx
+
+    # Calculate metrics for exact matches
+    precision_exact = tp_exact / (tp_exact + fp_exact)
+    recall_exact = tp_exact / (tp_exact + fn_exact)
+    f1_score_exact = 2 * (precision_exact * recall_exact) / (precision_exact + recall_exact)
+    accuracy_exact = tp_exact / (tp_exact + fn_exact)
+
+    # Calculate metrics for approximate matches (includes exact matches)
+    precision_approx = (tp_exact + tp_approx) / (tp_exact + tp_approx + fp_approx)
+    recall_approx = (tp_exact + tp_approx) / (tp_exact + tp_approx + fn_approx)
+    f1_score_approx = 2 * (precision_approx * recall_approx) / (precision_approx + recall_approx)
+    accuracy_approx = (tp_exact + tp_approx) / (tp_exact + tp_approx + fn_approx)
+    results = {
+        'exact_match': {
+            'count': tp_exact,
+            'percentage': tp_exact / total_entities,
+            'accuracy': accuracy_exact,
+            'precision': precision_exact,
+            'recall': recall_exact,
+            'f1_score': f1_score_exact
+        },
+        'approximate_match': {
+            'count': tp_approx,
+            'percentage': tp_approx / total_entities,
+            'accuracy': accuracy_approx,
+            'precision': precision_approx,
+            'recall': recall_approx,
+            'f1_score': f1_score_approx
+        },
+        'non-match': {
+            'count': fn_approx,
+            'percentage': fn_approx / total_entities
+        }
+    }
+    return results
+
+data = load_dataset('docred', trust_remote_code=True)
+datasets = [
+    ('train_annotated', 3035),
+    ('train_distant', 101873)
+]
+
+model_name = 'dslim/distilbert-NER'
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+ner_pipeline = pipeline('ner', model=model_name, tokenizer=tokenizer)
+
+N = 3000
+mismatches = []
+approx_matches = []
+results_all = {}
+results_all['number_of_instances'] = N
+
+tp_exact_all, fp_exact_all, fn_exact_all = 0, 0, 0
+tp_approx_all, fp_approx_all, fn_approx_all = 0, 0, 0
+
+for d, nrows in datasets:
+    print(f"Investigating first {N} random instances of {d} ({nrows} instances in total)")
+    mismatches.append(f"{d}:")
+    approx_matches.append(f"{d}:")
+
+    tp_exact, fp_exact, fn_exact = 0, 0, 0
+    tp_approx, fp_approx, fn_approx = 0, 0, 0
+
+    random_permutation = np.random.permutation(nrows)[:N]
+    ds = pd.DataFrame(data[d])
+    
+    for i, index in enumerate(random_permutation):
+        if i % 50 == 0:
+            print('Iteration:', i)
+
+        title, sents, ground_truth, _, _, _, _, _ = get_info(ds.iloc[index])
+        text = join_text(sents)
+        result = ner_pipeline(text)
+        merged_result = merge_result(result, model_name)
+        predicted = combine_entities(merged_result)
+
+        gold = [(gt['name'], gt['type']) for gt in ground_truth if gt['name']]
+        preds = [(p['word'], p['entity']) for p in predicted if p['entity']]
+        
+        for g in gold:
+            if g in preds:
+                tp_exact += 1
+                tp_approx += 1
+            elif get_approx_match(g, preds):
+                tp_approx += 1
+                fn_exact += 1
+                approx_matches.append(f"{g}: {get_approx_match(g, preds)}")
+            else:
+                fn_approx += 1
+                fn_exact += 1
+                mismatches.append('gold: ' + str(g))
+                mismatches.append('preds:')
+                for p in preds:
+                    mismatches.append(str(p))
+    
+        fp_exact += len([p for p in preds if p not in gold])
+        fp_approx += len([p for p in preds if p not in gold and get_approx_match(p, gold)])
+
+    results_all[d] = get_results_table(tp_exact, fp_exact, fn_exact, tp_approx, fp_approx, fn_approx)
+
+    tp_exact_all += tp_exact
+    fp_exact_all += fp_exact
+    fn_exact_all += fn_exact
+    tp_approx_all += tp_approx
+    fp_approx_all += fp_approx
+    fn_approx_all += fn_approx
+
+print('Calculating totals...')
+
+results_all['total'] = get_results_table(tp_exact_all, fp_exact_all, fn_exact_all, tp_approx_all, fp_approx_all, fn_approx_all)
+
+print('Done. Writing files...')
+
+DATE = '240716'
+
+with open('results/' + DATE + '_docred_mismatches.txt', 'w') as file:
+    for mismatch in mismatches:
+        file.write(str(mismatch) + '\n')
+
+with open('results/' + DATE + '_docred_approximate_matches.txt', 'w') as file:
+    for approx_match in approx_matches:
+        file.write(str(approx_match) + '\n')
+
+with open('results/' + DATE + '_docred.json', 'w') as f:
+    json.dump(results_all, f, indent=4)
