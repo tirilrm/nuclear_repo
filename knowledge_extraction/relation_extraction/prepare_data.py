caa4109e8d2ca4dbe521beb7ef96fba396d83397
Tiril Mageli <tiril.mageli23@imperial.ac.uk>
2024-07-23 12:19:33 +0200

    Data preparation for RE training

---
+++ knowledge_extraction/relation_extraction/prepare_data.py
@@ -0,0 +1,113 @@
+from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
+from torch import nn
+import os
+import json
+import torch
+import pickle
+import re
+from _RE import merge_result, combine_entities
+
+model_name = 'dslim/distilbert-NER'
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+ner_model = AutoModelForTokenClassification.from_pretrained(model_name)
+
+ner_pipeline = pipeline('ner', model=ner_model, tokenizer=tokenizer)
+
+def identify_entities(paragraphs, custom_keywords, model_name = 'dslim/distilbert-NER'):
+    '''
+    Finds all entities in a given paraph using 
+    1) The pretrained and finetuned DistilBERT extractor, and
+    2) The keyword dictionary
+
+    Returns a list of entities where each entity is indicated by:
+    - 'word': the name of the entity (e.g. 'Barack Obama')
+    - 'entity': the type of entity (for BERT-based models: one of ORG, LOC, PER, MISC)
+    - start and end character index of the word
+    '''
+    ner_entities = []
+    keyword_entities = []
+    for paragraph in paragraphs:
+        raw = ner_pipeline(paragraph)
+        raw_merged = merge_result(raw, model_name)
+        ner_results = combine_entities(raw_merged)
+        
+        # Add standard NER results
+        for result in ner_results:
+            entity = {
+                'word': result['word'],
+                'entity': result['entity'],
+                'start': result['start'],
+                'end': result['end']
+            }
+            ner_entities.append(entity)
+        
+        # Add custom keyword results
+        for label, terms in custom_keywords.items():
+            for term in terms:
+                for match in re.finditer(term, paragraph):
+                    entity = {
+                        'word': term,
+                        'entity': label,
+                        'start': match.start(),
+                        'end': match.end()
+                    }
+                    keyword_entities.append(entity)
+
+    result = {
+        'Context': paragraphs ,
+        'NER': ner_entities, 
+        'Keywords': keyword_entities
+    }
+    return result
+
+def create_entity_pairs(entities):
+    '''
+    `entities` contains separate lists of NER-identified entities and keyword-matched entities
+    Returns a set of heterogeneous entity pairs for form (('name', 'entity'), ('name', 'entity))
+    For N entities, `create_entity_pairs` returns a list of maximum length N^2-N
+    '''
+    joined = []
+    joined.extend(entities['NER'])
+    joined.extend(entities['Keywords'])
+    pairs = set()
+    
+    for i, entity1 in enumerate(joined):
+        for j, entity2 in enumerate(joined):
+            if i != j:
+                # Use a sorted tuple to avoid duplicates where order does not matter
+                pair = tuple(sorted((tuple([entity1['word'], entity1['entity']]), tuple([entity2['word'], entity2['entity']]))))
+                pairs.add(pair)
+                
+    return list(pairs)
+
+
+
+current_directory = os.path.abspath(os.path.dirname(__file__))
+two_layers_up = os.path.dirname(os.path.dirname(current_directory))
+#new_path = os.path.join(two_layers_up, 'scraping', 'articles')
+#print(new_path)
+
+file = open('scraping/articles/filtered_articles.json', 'r')
+articles = file.read()
+articles = json.loads(articles)
+
+with open('keyword_matching/directory.pkl', 'rb') as file:
+    custom_keywords = pickle.load(file)
+
+print(custom_keywords)
+
+context_and_pairs = []
+for article in articles[:2]:
+    print(article)
+    elems = {}
+
+    context = article['text']
+    entities = identify_entities(context, custom_keywords)
+    pairs = create_entity_pairs(entities)
+
+    elems['context'] = article['text']
+    elems['pairs'] = pairs
+    context_and_pairs.append(elems)
+
+with open('data/context_and_pairs.pkl', 'wb') as file:
+    pickle.dump(context_and_pairs, file)
