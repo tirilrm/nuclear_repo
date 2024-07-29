87e688986b1d03e05449da1def4f375152f01f99
Tiril Mageli <tiril.mageli23@imperial.ac.uk>
2024-07-29 14:50:22 +0200

    Developing custom loss function

--- knowledge_extraction/relation_extraction/REBRNN.py
+++ knowledge_extraction/relation_extraction/REBRNN.py
@@ -2,11 +2,12 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 
 from datasets import load_dataset
 from transformers import pipeline, AutoTokenizer
 
+from difflib import SequenceMatcher
 import pickle
 import json
 import pandas as pd
@@ -14,7 +15,14 @@ import numpy as np
 import Levenshtein
 import re
 import time
+import os
 
+'''
+NOTES
+- hidden_size must be multiplied by 2 since it's bidirectional; one layer going forward and one going backward
+'''
+
+'''
 # Set device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -22,8 +30,12 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model_name = 'dslim/distilbert-NER'
 
 # Custom keywords
-with open('keyword_matching/directory.pkl', 'rb') as file:
-    keywords = pickle.load(file)
+try:
+    with open('keyword_matching/directory.pkl', 'rb') as file:
+        keywords = pickle.load(file)
+except FileNotFoundError or FileExistsError:
+    with open('directory.pkl', 'rb') as file:
+        keywords = pickle.load(file)
 
 # Hyperparameters
 input_size = 512
@@ -32,21 +44,17 @@ hidden_size = 256       # may require tuning
 num_classes = 97        # 96 different relations plus '0' for no relation
 learning_rate = 0.001   # may require tuning
 batch_size = 64
-num_epochs = 5
-
-'''
-NOTES
-- hidden_size must be multiplied by 2 since it's bidirectional; one layer going forward and one going backward
-'''
+num_epochs = 5'''
 
 class RelationExtractorBRNN(nn.Module):
 
-    def __init__(self, input_size, hidden_size, num_layers, num_classes, model_name, custom_keywords=None, threshold=0.7):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes, batch_size, model_name, custom_keywords=None, threshold=0.7):
         super(RelationExtractorBRNN, self).__init__()
         
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.num_layers = num_layers
+        self.batch_size = batch_size
         self.model_name = model_name
         
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, bidirectional=True) #check if batch_first=True is required
@@ -57,6 +65,10 @@ class RelationExtractorBRNN(nn.Module):
 
         self.custom_keywords = custom_keywords
         self.threshold = threshold
+
+        self.train_loader = None
+        self.val_loader = None
+        self.test_loader = None
 
     def forward(self, x):
 
@@ -72,6 +84,16 @@ class RelationExtractorBRNN(nn.Module):
 
         return out
     
+    def set_data_loaders(self, train_preprocessed, val_preprocessed, test_preprocessed):
+
+        train_dataset = RelationExtractionDataset(train_preprocessed)
+        val_dataset = RelationExtractionDataset(val_preprocessed)
+        test_dataset = RelationExtractionDataset(test_preprocessed)
+
+        self.train_loader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
+        self.val_loader = DataLoader(val_dataset, batch_size=self.batch_size, shuffle=False)
+        self.test_loader = DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False)
+
     def tokenize_input(self, context):
         inputs = self.tokenizer(
             context,
@@ -125,7 +147,7 @@ class RelationExtractorBRNN(nn.Module):
             if padding == 0:
                 count += 1
         end = time.time()
-        print(f'Preprocessing took {(end-start)/60:.2f} min. {count} instances ({(count/length)*100:.2f}%) exceeded max length.')
+        print(f'Preprocessing took {(end-start)/60:.2f} minutes. {count} instances ({(count/length)*100:.2f}%) exceeded max length.')
         return data
 
     ##############################################
@@ -408,19 +430,75 @@ class RelationExtractorBRNN(nn.Module):
         tagged_text = " [SEP] ".join(tagged_sents)
 
         return tagged_sents, tagged_text
+
+class RelationExtractionDataset(Dataset):
+    def __init__(self, preprocessed_data):
+        self.data = preprocessed_data
     
-model = RelationExtractorBRNN(
-    input_size, hidden_size, num_layers, num_classes, 
-    model_name, keywords, threshold=0.7
-)
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        item = self.data[idx]
+        input_ids = torch.tensor(item['input']['input_ids'][0])
+        attention_mask = torch.tensor(item['input']['attention_mask'][0])
+        labels = torch.tensor(item['triplets'])
+        entity_pairs = item['entity_pairs']
+        tagged_sents = item['sents']
 
-train, _, test, validation = model.load_data(get_distant=False)
+        return {
+            'input_ids': input_ids,
+            'attention_mask': attention_mask,
+            'labels': labels,
+            'entity_pairs': entity_pairs,
+            'tagged_sents': tagged_sents
+        }
 
-print('Preprocessing training data...')
-train_preprocessed = model.preprocessor(train)
+class CustomLoss(nn.Module):
+    def __init__(self, threshold=0.8):
+        super(CustomLoss, self).__init__()
+        self.threshold = threshold
+        self.cross_entropy = nn.CrossEntropyLoss()
 
-print('Preprocessing testing data...')
-test_preprocessed = model.preprocessor(test)
+    def similarity(a, b):
+        return SequenceMatcher(None, a, b).ratio()
 
-print('Preprocessing validation data...')
-validation_preprocessed = model.preprocessor(validation)
+    def compute_instance_loss(self, entity_pairs, predictions, triplets):
+        instance_loss = 0
+        output = []
+
+        for i in range(len(predictions)):
+            prediction = predictions[i]
+            pred_head, pred_tail = entity_pairs[i]
+            
+            for triplet in triplets:
+                best_similarity = 0
+                best_relation = '0'
+
+                gold_heads, gold_relation, gold_tails = triplet
+                head_similarity = max(self.similarity(pred_head, gold_head) for gold_head in gold_heads)
+                tail_similarity = max(self.similarity(pred_tail, gold_tail) for gold_tail in gold_tails)
+
+                if head_similarity > self.threshold and tail_similarity > self.threshold:
+                    avg_similarity = (head_similarity + tail_similarity) / 2
+
+                    if avg_similarity > best_similarity:
+                        best_similarity = avg_similarity
+                        best_relation = gold_relation
+                
+            output.append[[prediction, best_relation]]
+
+        for pred, best_relation in output:
+            target = torch.tensor(int(best_relation), dtype=torch.long, device=predictions.device)
+            loss = self.cross_entropy(pred.unsqueeze(0), target.unsqueeze(0))
+            instance_loss += loss
+
+        return instance_loss / len(output)
+    
+    def forward(self, batch_entity_pairs, batch_predictions, batch_triplets):
+        batch_loss = 0
+        for entity_pairs, predicitons, triplets in zip(batch_entity_pairs, batch_predictions, batch_triplets):
+            instance_loss = self.compute_instance_loss(entity_pairs, predicitons, triplets)
+            batch_loss += instance_loss
+        
+        return batch_loss/len(batch_predictions)
